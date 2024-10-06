a28edf859c5dd29de71c9d1ce79c9042ce4754d8
sevirekardes <sevire_kardes23@erdogan.edu.tr>
2024-10-06 14:07:15 +0300

    Update hello_python.py
    memleket istendi

--- python.py/hello_python.py
+++ python.py/hello_python.py
@@ -2,10 +2,9 @@ a='hello world'
 print(a)
 isim = input("Lütfen isminizi girin: ")
 print("Merhaba, "  + isim + "!")
-bolum = input("Hangi bölümü okuyorsunuz? ")
-print(f"{bolum} bölümünü okuyorsunuz.")
-yas = input("Lütfen yaşinizi girin: ")
-print(f"Girdiğiniz yaş: {yas}")
+memleket = input("Nerelisiniz? ")
+print(f"{isim}, {memleket} bölgesindensiniz.")
+
 
 
 

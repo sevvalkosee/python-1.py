93a685e3dcaba293756c4b6f039827232a479b62
sevirekardes <sevire_kardes23@erdogan.edu.tr>
2024-10-06 13:59:59 +0300

    Update hello_python.py
    silindi

--- python.py/hello_python.py
+++ python.py/hello_python.py
@@ -2,13 +2,10 @@ a='hello world'
 print(a)
 isim = input("Lütfen isminizi girin: ")
 print("Merhaba, "  + isim + "!")
-<<<<<<< HEAD
 bolum = input("Hangi bölümü okuyorsunuz? ")
 print(f"{bolum} bölümünü okuyorsunuz.")
-=======
 yas = input("Lütfen yaşinizi girin: ")
 print(f"Girdiğiniz yaş: {yas}")
 
 
->>>>>>> 59145edbdc1c92e88bac8de4646640621aa59e24
 

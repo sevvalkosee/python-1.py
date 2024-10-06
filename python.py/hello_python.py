525834d4c78072d0ad9c75b0d35442c97fd7315d
sevirekardes <sevire_kardes23@erdogan.edu.tr>
2024-10-06 13:51:58 +0300

    Update hello_python.py
    bölüm istendi.

--- python.py/hello_python.py
+++ python.py/hello_python.py
@@ -2,3 +2,5 @@ a='hello world'
 print(a)
 isim = input("Lütfen isminizi girin: ")
 print("Merhaba, "  + isim + "!")
+bolum = input("Hangi bölümü okuyorsunuz? ")
+print(f"{bolum} bölümünü okuyorsunuz.")

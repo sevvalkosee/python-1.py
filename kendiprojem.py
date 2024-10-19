6957a9a81a158dfefad1325ab833ec04b46cb89c
sevirekardes <sevire_kardes23@erdogan.edu.tr>
2024-10-19 15:29:51 +0300

    deneme rastgele

---
+++ kendiprojem.py
@@ -0,0 +1,11 @@
+from cgi import print_form
+
+renkler= ["beyaz","kırmızı","siyah"]
+print (renkler)
+renkler.sort(reverse=True)
+print(renkler)
+
+
+
+
+
---
+++ kendiprojem2.py
@@ -0,0 +1,10 @@
+a="python"
+b="pytho"
+b+="n"
+
+if a is b:
+    print("a=b")
+else:
+    print("a!=b")
+
+

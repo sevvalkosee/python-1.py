9adee62b95eb04739256707412d1820b2067dd9e
sevirekardes <sevire_kardes23@erdogan.edu.tr>
2024-10-22 19:29:45 +0300

    toplama işlemi

--- hesapmakinesi.py
+++ hesapmakinesi.py
@@ -1,3 +1,7 @@
 print("HESAP MAKİNESİ")
-def toplama(a,b):
+def toplam(a,b):
     return a+b
+a=int(input("birinci sayı gir"))
+b=int(input("ikinci sayıyı gir"))
+sonuc= toplam(a,b)
+print(f"toplam sonuc: {sonuc}")

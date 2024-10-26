7eeccdaa85e445f2376465651aff1eed94190313
sevvalkosee <sevval_kose23@erdogan.edu.tr>
2024-10-26 22:28:27 +0300

    test
    çıkarma işlemi

---
+++ çıkarma.py
@@ -0,0 +1,8 @@
+def cikar(sayi1, sayi2):
+    return sayi1 - sayi2
+
+sayi1 = float(input("Birinci sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
+
+sonuc = cikar(sayi1,sayi2)
+print("Sonuç:", sonuc)
